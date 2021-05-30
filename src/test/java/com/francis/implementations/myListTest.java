dd85bc5061e78d3354214df09a6b0a00580e8b81
EmmanuelFrancis95 <mannyfrancis95@gmail.com>
2021-05-30 18:03:05 +0100

    Test update

---
+++ src/test/java/com/francis/implementations/myListTest.java
@@ -0,0 +1,26 @@
+package com.francis.implementations;
+
+import com.francis.interfaces.IList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class myListTest {
+
+    @Test
+    public void test_emptyList_hasZeroSize() {
+        IList list = new myList();
+        assertEquals(list.getSize(), 0);
+    }
+
+    @Test
+    public void test_insertItems_preservesInsertOrder() {
+        IList list = new myList();
+
+        list.add("apple");
+        list.add("orange");
+
+        assertEquals(list.get(0), "apple");
+        assertEquals(list.get(1), "orange");
+    }
+}
